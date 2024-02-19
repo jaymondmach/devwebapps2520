044301c7a368662d947217d853f8671f619fc42a
jaymondm <jaymondmach@gmail.com>
2024-02-19 12:53:20 -0800

    third change

--- Lab 6/starter 6 2/main.js
+++ Lab 6/starter 6 2/main.js
@@ -1,15 +1,32 @@
 const path = require("path");
+
 /*
  * Project: Milestone 1
  * File Name: main.js
  * Description:
  *
- * Created Date:
- * Author:
+ * Created Date: February 19, 2024
+ * Author: Jaymond Mach
  *
  */
 
 const IOhandler = require("./IOhandler");
+
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
+
+(async () => {
+  try {
+    await IOhandler.unzip(zipFilePath, pathUnzipped);
+    console.log("Zip file decompressed successfully!");
+
+    const pngFiles = await IOhandler.readDir(pathUnzipped);
+    console.log("Found PNG files:", pngFiles);
+
+    await IOhandler.grayscale(pathUnzipped, pathProcessed);
+    console.log("PNG files converted to grayscale successfully!");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+})();
